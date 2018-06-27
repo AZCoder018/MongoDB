bd651243f448187e2592f26894ccfb87a6d01758
AZCoder018 <jason.meadows.esq@hotmail.com>
2018-06-27 01:32:26 -0700

    revised  routes.js

--- config/routes.js
+++ config/routes.js
@@ -5,41 +5,35 @@ var articlesController = require("../controllers/articles");
 
 module.exports = function(router) {
 
-
-  router.get("/", function(req, res) {
-      Article.find({saved: false}, function(error, found) {
-          if (error) {
-              console.log(error);
-          } else if (found.length === 0) {
-              res.render("empty")
-          } else {
-
+router.get("/", function(req, res) {
+    Article.find({saved: false}, function(error, found) {
+        if (error) {
+         console.log(error);
+        } else if (found.length === 0) {
+         res.render("empty")
+        } else {
             var hbsObject = {
-                articles: found
-            };
+            articles: found
+        };
             res.render("index", hbsObject);
-
-          }
-      });
+        }
+    });
   });
 
-  router.get("/api/fetch", function(req, res) {
-
-      // scrapes articles and saves unique ones to database
-      articlesController.fetch(function(err, docs) {
-          //lets user know if there were new articles or not
-          if (!docs || docs.insertedCount === 0) {
-              res.json({message: "No new articles today. Check back tomorrow!"});
-          }
-          else {
-              res.json({message: "Added " + docs.insertedCount + " new articles!"});
-
+router.get("/api/fetch", function(req, res) {
+    
+//Scrapes articles and saves to database; lets user know if there are new articles or not
+    articlesController.fetch(function(err, docs) {
+        if (!docs || docs.insertedCount === 0) {
+            res.json({message: "No new articles today. Check back tomorrow!"});
+        }
+        else {
+            res.json({message: "Added " + docs.insertedCount + " new articles!"});
           }
       });
   });
 
   router.get("/saved", function(req, res) {
-
       articlesController.get({saved: true}, function(data) {
           var hbsObject = {
             articles: data
@@ -48,35 +42,35 @@ module.exports = function(router) {
       });
   });
 
-  //for saving or unsaving articles
+//Save or unsave articles
   router.patch("/api/articles", function(req, res) {
-
       articlesController.update(req.body, function(err, data) {
           res.json(data);
       });
   });
 
+//Query to find matching id; populate notes associated with id; execute query
   router.get('/notes/:id', function (req, res) {
-      //Query to find the matching id to the passed in it
       Article.findOne({_id: req.params.id})
-          .populate("note") //Populate all of the notes associated with it
-          .exec(function (error, doc) { //execute the query
+          .populate("note") 
+          .exec(function (error, doc) { 
               if (error) console.log(error);
               else {
                   res.json(doc);
-              }
-          });
+            }
+        });
   });
 
+//Save new Note to db
   router.post('/notes/:id', function (req, res) {
       var newNote = new Note(req.body);
-      //save newNote to the db
       newNote.save(function (err, doc) {
           if (err) console.log(err);
           Article.findOneAndUpdate(
-              {_id: req.params.id}, // find the _id by req.params.id
-              {push: {note: doc._id}}, //push to the notes array
-              {new: true},
+//Find the _id by req.params.id; push to notes array
+            {_id: req.params.id}, 
+            {push: {note: doc._id}}, 
+            {new: true},
               function(err, newdoc){
                   if (err) console.log(err);
                   res.send(newdoc);
@@ -84,10 +78,12 @@ module.exports = function(router) {
       });
   });
 
-  router.get('/deleteNote/:id', function(req, res){
-      Note.remove({"_id": req.params.id}, function(err, newdoc){
-          if(err) console.log(err);
-          res.redirect('/saved'); //redirect to reload the page
+router.get('/deleteNote/:id', function(req, res){
+    Note.remove({"_id": req.params.id}, function(err, newdoc){
+        if(err) console.log(err);
+          
+//Need to redirect to reload page
+          res.redirect('/saved'); 
       });
   });
 
